c5354a8c1f857b1323df7a9f681dad5798d0ee22
polopalay <60103759+polopalay@users.noreply.github.com>
2020-06-05 15:55:41 +0700

    update

---
+++ VNPost.Models/ViewModels/HeaderViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VNPost.Models.ViewModels
+{
+    public class HeaderViewModel : PageModel
+    {
+        public string Name { get; set; }
+        public void OnGet()
+        {
+            Name = "Test product";
+        }
+    }
+}
---
+++ VNPost/Areas/Component/Controllers/HeaderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VNPost.Models.ViewModels;
+
+namespace VNPost.Areas.Component.Controllers
+{
+    [Area("Component")]
+    public class HeaderController : Controller
+    {
+        public IActionResult Index()
+        {
+            HeaderViewModel headerView = new HeaderViewModel();
+            headerView.Name = "Hello";
+            return View(headerView);
+        }
+    }
+}
