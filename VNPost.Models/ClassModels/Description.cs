132df251507875b79df0b53b5531711475574bce
polopalay <60103759+polopalay@users.noreply.github.com>
2020-06-05 13:55:18 +0700

    Add project files.

---
+++ VNPost.Models/ClassModels/Description.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VNPost.Models.ClassModels
+{
+    public class Description
+    {
+        [Key]
+        public int Id { get; set; }
+        public string key { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ VNPost/wwwroot/js/component/header.js
@@ -0,0 +1,29 @@
+﻿const menuBarAccessory = document.getElementById("menu-bar-accessory");
+const menuBar = document.getElementById("menu-bar");
+const hamburgerBar = document.getElementById("hamburger");
+const LinkBar = document.getElementById("link-bar");
+
+menuBarAccessory.addEventListener("click", () => {
+    console.log(menuBar.style.display);
+    if (menuBar.style.display === "none" || menuBar.style.display === "") {
+        menuBar.style.display = "flex";
+    } else {
+        menuBar.style.display = "none";
+    }
+});
+hamburgerBar.addEventListener("click", () => {
+    console.log(LinkBar.style.display);
+    if (LinkBar.style.display === "none" || LinkBar.style.display === "") {
+        LinkBar.style.display = "flex";
+    } else {
+        LinkBar.style.display = "none";
+    }
+});
+window.onresize = function (event) {
+    console.log(window.innerWidth);
+    if (window.innerWidth >= 860) {
+        LinkBar.style.display = "flex";
+    } else {
+        LinkBar.style.display = "none";
+    }
+};
