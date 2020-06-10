56fe698ff2376fef24bbd17dd9f2c4addfafde3e
polopalay <60103759+polopalay@users.noreply.github.com>
2020-06-10 18:40:11 +0700

    update main

--- VNPost/wwwroot/js/main/index.js
+++ VNPost/wwwroot/js/main/index.js
@@ -1,6 +1,6 @@
-﻿$("#slider").mouseover(function () {
-    $("#slider-link").css("display", "flex");
-});
-$("#slider").mouseout(function () {
-    $("#slider-link").css("display", "none");
-});
+﻿//$("#slider").mouseover(function () {
+//    $("#slider-link").css("display", "flex");
+//});
+//$("#slider").mouseout(function () {
+//    $("#slider-link").css("display", "none");
+//});
