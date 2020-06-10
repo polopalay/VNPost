a9862bd5dc5bca2fe682f8614ba11a650c1114fc
polopalayyy <51782822+polopalayyy@users.noreply.github.com>
2020-06-10 10:18:56 +0700

    update st

---
+++ VNPost.Models/ViewModels/HomeVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using VNPost.Models.Entity;
+
+namespace VNPost.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public HomeVM(List<MenuLink> links)
+        {
+            Links = links;
+        }
+
+        public List<MenuLink> Links { get; set; }
+    }
+}
