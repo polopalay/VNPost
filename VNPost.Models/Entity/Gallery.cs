691cc6fd576e854b1a1d03e3508f0855f38cffcf
polopalayyy <51782822+polopalayyy@users.noreply.github.com>
2020-06-11 09:36:55 +0700

    done gallery

---
+++ VNPost.Models/Entity/Gallery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace VNPost.Models.Entity
+{
+    public class Gallery
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateWrite { get; set; }
+    }
+}
