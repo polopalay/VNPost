06d1c31a3343b54928e16422bd9504df874bb989
polopalayyy <51782822+polopalayyy@users.noreply.github.com>
2020-06-12 22:18:13 +0700

    update

---
+++ VNPost/Areas/Posts/Controllers/ServiceController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VNPost.DataAccess.Repository.IRepository;
+using VNPost.Models.Entity;
+
+namespace VNPost.Areas.Posts.Controllers
+{
+    [Area("Posts")]
+    public class ServiceController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ServiceController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult List(int id)
+        {
+            List<Post> posts = _unitOfWork.Post.GetAll().Where(p => p.CategoryId == id).ToList();
+            return View(posts);
+        }
+    }
+}
