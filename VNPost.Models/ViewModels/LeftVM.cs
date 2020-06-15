b62078832e9e4bac75ea424f8ebd475f1d505285
polopalayyy <51782822+polopalayyy@users.noreply.github.com>
2020-06-16 00:39:02 +0700

    done article

---
+++ VNPost.Models/ViewModels/LeftVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using VNPost.Models.Entity;
+
+namespace VNPost.Models.ViewModels
+{
+    public class LeftVM
+    {
+        public LeftVM(List<Columnist> columnist, List<ColumnistItem> columnistItem, Dictionary<string, string> listMenuItem,
+           int columnistId, int columnistItemId)
+        {
+            Columnist = columnist;
+            ListMenuItem = listMenuItem;
+            ColumnistItem = columnistItem;
+            ColumnistId = columnistId;
+            ColumnistItemId = columnistItemId;
+        }
+
+        public Dictionary<string, string> ListMenuItem { get; set; }
+        public List<Columnist> Columnist { get; set; }
+        public List<ColumnistItem> ColumnistItem { get; set; }
+        public int ColumnistId { get; set; }
+        public int ColumnistItemId { get; set; }
+    }
+}
---
+++ VNPost/Areas/Components/LeftViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using VNPost.DataAccess.Repository.IRepository;
+using VNPost.Models.Entity;
+using VNPost.Models.ViewModels;
+
+namespace VNPost.Areas.Components
+{
+    public class LeftViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public LeftViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IViewComponentResult Invoke([FromQuery] int columnistId, [FromQuery] int columnistItemId)
+        {
+            Dictionary<string, string> listMenuItem = new Dictionary<string, string>();
+            foreach (MenuItem menuItem in _unitOfWork.MenuItem.GetAll())
+            {
+                listMenuItem.Add(menuItem.Key, menuItem.Value);
+            }
+            List<Columnist> columnist = _unitOfWork.Columnist.GetAll().ToList();
+            List<ColumnistItem> columnistItems = _unitOfWork.ColumnistItem.GetAll().ToList();
+            LeftVM articleVM = new LeftVM(columnist, columnistItems, listMenuItem, columnistId, columnistItemId);
+            return View(articleVM);
+        }
+    }
+}
