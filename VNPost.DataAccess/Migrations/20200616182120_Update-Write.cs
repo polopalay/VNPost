dfa46db87e43a728302619a587c4728e26af8bdc
polopalayyy <51782822+polopalayyy@users.noreply.github.com>
2020-06-17 01:22:51 +0700

    done

---
+++ VNPost.DataAccess/Migrations/20200616182120_Update-Write.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VNPost.DataAccess.Migrations
+{
+    public partial class UpdateWrite : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MenuLinks",
+                keyColumn: "Id",
+                keyValue: 12,
+                columns: new[] { "Link", "Value" },
+                values: new object[] { "/Posts/Write/WriteArticle", "Viết Bài" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MenuLinks",
+                keyColumn: "Id",
+                keyValue: 12,
+                columns: new[] { "Link", "Value" },
+                values: new object[] { "#", "Email" });
+        }
+    }
+}
---
+++ VNPost/Areas/Posts/Controllers/WriteController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using VNPost.DataAccess.Repository.IRepository;
+using VNPost.Models.Entity;
+using VNPost.Models.ViewModels;
+
+namespace VNPost.Areas.Posts.Controllers
+{
+    [Area("Posts")]
+    public class WriteController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public WriteController(IUnitOfWork unitOfWork, SignInManager<IdentityUser> signInManager)
+        {
+            _unitOfWork = unitOfWork;
+            _signInManager = signInManager;
+        }
+        public IActionResult WriteArticle()
+        {
+            if (!_signInManager.IsSignedIn(User))
+            {
+                return Redirect("/Identity/Account/Login");
+            }
+            else
+            {
+                List<Columnist> columnists = _unitOfWork.Columnist.GetAll().ToList();
+                List<ColumnistItem> columnistItems = _unitOfWork.ColumnistItem.GetAll().ToList();
+                Article article = new Article();
+                WriteArticleVM writeArticleVM = new WriteArticleVM(article, columnists, columnistItems);
+                return View(writeArticleVM);
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult WriteArticle(Article article)
+        {
+            if (!_signInManager.IsSignedIn(User))
+            {
+                return Redirect("/Identity/Account/Login");
+            }
+            string s = "";
+            string rawText = "data:image/";
+            int begin = article.Content.IndexOf(rawText);
+            if (begin != -1)
+            {
+                s = article.Content;
+                s = s.Substring(begin, s.IndexOf('"', begin + rawText.Length) - begin);
+            }
+            else
+            {
+                rawText = "src=\"";
+                begin = article.Content.IndexOf(rawText);
+                if (begin != -1)
+                {
+                    s = article.Content;
+                    s = s.Substring(begin + 5, s.IndexOf('"', begin + rawText.Length) - begin);
+                }
+            }
+            article.DescriptionImg = s;
+            article.DateCreate = DateTime.Now;
+            article.View = 0;
+            _unitOfWork.Article.Add(article);
+            _unitOfWork.Save();
+            return Redirect("/");
+        }
+    }
+}
